4d2ac5b448d915a7bb7e2d1ca7355d52e9be6e8e
N'ZAOU Renaud <r.nzaou@sncf.fr>
2024-01-11 12:31:21 +0100

    wip

--- src/main/java/fr/sncf/d2d/colibri/ColibriApplication.java
+++ src/main/java/fr/sncf/d2d/colibri/ColibriApplication.java
@@ -3,8 +3,6 @@ package fr.sncf.d2d.colibri;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import fr.sncf.d2d.colibri.colis.rest.CreateColisBody;
@@ -13,15 +11,6 @@ import fr.sncf.d2d.colibri.colis.rest.CreateColisBody;
 public class ColibriApplication {
 
 	public static void main(String[] args) {
-		final var context = SpringApplication.run(ColibriApplication.class, args);
-		final var objectMapper = context.getBean(ObjectMapper.class);
-
-		try {
-			System.out.println(objectMapper.readValue("{ \"toto\": \"bablablab\" }", CreateColisBody.class));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		SpringApplication.run(ColibriApplication.class, args);
 	}
 }
